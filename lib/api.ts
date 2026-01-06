194f4996799a486b2829280fbc7924d28928a01c
bohdan_berest <berestbodi@gmail.com>
2026-01-06 17:32:12 +0200

    first commit

---
+++ lib/api.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { Note, NoteFormValues } from "@/types/note";
+
+const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+const options = {
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${myKey}`,
+  },
+};
+
+export interface NoteRes {
+  notes: Note[];
+  totalPages: number;
+}
+
+const url = "https://notehub-public.goit.study/api/notes";
+
+export async function fetchNotes(
+  currentPage: number,
+  query?: string
+): Promise<NoteRes> {
+  const res = await axios.get<NoteRes>(url, {
+    ...options,
+    params: { page: currentPage, perPage: 12, search: query },
+  });
+  return res.data;
+}
+
+export async function createNote(values: NoteFormValues): Promise<Note> {
+  const res = await axios.post<Note>(url, values, { ...options });
+
+  return res.data;
+}
+
+export async function deleteNote(id: string): Promise<Note> {
+  const res = await axios.delete<Note>(url + `/${id}`, { ...options });
+
+  return res.data;
+}
+
+export async function fetchNoteById(id: string) {
+  const res = await axios.get(url + `/${id}`, { ...options });
+
+  return res.data;
+}
